a226d39f052a7f3c9c81e5ccd3c103fb6c3199a5
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-06-23 13:38:33 -0400

    Added files

--- day09-react-and-tailwind-mini-lecture/job-app-tracker/src/App.jsx
+++ day09-react-and-tailwind-mini-lecture/job-app-tracker/src/App.jsx
@@ -3,16 +3,16 @@ import jobs from "./jobs";
 import "./App.css";
 
 function App() {
-  const jobCards = jobs.map((job, i) => {
-    return <JobCard job={job} key={i} />;
-  });
+    const jobCards = jobs.map((job, i) => {
+        return <JobCard job={job} key={i} />;
+    });
 
-  return (
-    <div className="container">
-      <h1>Job Application Tracker</h1>
-      {jobCards}
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Job Application Tracker</h1>
+            {jobCards}
+        </div>
+    );
 }
 
 export default App;
