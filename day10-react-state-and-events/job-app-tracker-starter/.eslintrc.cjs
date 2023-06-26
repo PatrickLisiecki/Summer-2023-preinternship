8392a8dd54fa4a291c1f4d39c706bd52c195acdc
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-06-26 10:46:53 -0400

    Updated

--- day10-react-state-and-events/job-app-tracker-starter/.eslintrc.cjs
+++ day10-react-state-and-events/job-app-tracker-starter/.eslintrc.cjs
@@ -1,15 +1,20 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+    env: { browser: true, es2020: true },
+    plugins: ["testing-library", "jest-dom", "vitest", "react-refresh"],
+    extends: [
+        "react-app",
+        "react-app/jest",
+        "plugin:testing-library/react",
+        "plugin:jest-dom/recommended",
+        "plugin:vitest/recommended",
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+    ],
+    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+    settings: { react: { version: "18.2" } },
+    rules: {
+        "react-refresh/only-export-components": "warn",
+    },
+};
---
+++ day10-react-state-and-events/job-app-tracker-starter/src/JobCard.test.jsx
@@ -0,0 +1,17 @@
+// JobCard.test.jsx
+import { test } from "vitest";
+import { render } from "@testing-library/react";
+import JobCard from "./JobCard";
+
+test("Renders without crashing", () => {
+    const job = {
+        image: { src: "image.jpg", alt: "job image" },
+        company: "Google",
+        title: "Software Engineer",
+        salary: "100k",
+        location: "New York",
+        postDate: "2022-06-20",
+    };
+
+    render(<JobCard job={job} />);
+});
