8d3ac0aba0c1ea70beabcfad9b3ba300b67090b4
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-07-10 13:53:50 -0400

    Updated

--- day18-cyclic-sort-and-fast-and-slow-pointers/fast-and-slow-pointers.js
+++ day18-cyclic-sort-and-fast-and-slow-pointers/fast-and-slow-pointers.js
@@ -5,7 +5,21 @@ class Node {
     }
 }
 
-function hasCycle(head) {}
+function hasCycle(head) {
+    let slow = head,
+        fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 module.exports = {
     hasCycle,
